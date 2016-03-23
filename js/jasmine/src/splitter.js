f175ddd6a20bbb0d1cf339502826c4fe114bf781
private <user@example.com>
2016-03-23 09:38:15 +0000

    Adds the splitter.

---
+++ js/jasmine/src/splitter.js
@@ -0,0 +1,24 @@
+function Splitter() {
+  this.numberOfGroups = 4;
+
+  this.getContentsFrom = function(textareaId) {
+    var textarea = document.getElementById(textareaId);
+    return textarea.value;
+  };
+
+  this.getAttendees = function(userInput) {
+    return String.split(userInput, "\n");
+  };
+
+  this.sort = function(attendees) {
+    var sorted = [];
+    while(attendees.length > 0) {
+      sorted.push(attendees.splice(0, this.numberOfGroups));
+    }
+    return sorted;
+  };
+
+  this.splitInGroups = function(textareaId) {
+    return this.sort(this.getAttendees(this.getContentsFrom(textareaId)));
+  }
+}
